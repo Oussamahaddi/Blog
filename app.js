0bd887e2a51a5c5196918ac6848951f1799358a0
Oussama Haddi <oussama.turboxxx1@gmail.com>
2023-10-09 02:12:17 +0100

    ajouter la fonction qui modifier la category avec ca page

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const flash = require("connect-flash");
+const session = require("express-session");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views")); 
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+
+app.use("/", require("./router"));
+
+module.exports = app;
--- router/index.js
+++ router/index.js
@@ -3,7 +3,6 @@ const { check } = require("express-validator");
 const router = express.Router();
 const blogsController = require("../controllers/blogs");
 const categoryController = require("../controllers/category");
-
 // blogs
 router.get('/', blogsController.index);
 router.get("/blog", blogsController.addBlogPage);
@@ -12,11 +11,11 @@ router.get("/singleBlog/:id", blogsController.singleBlog);
 router.get("/updateBlog/:id", blogsController.updateBlog);
 router.post("/saveUpdateBlog/:id", blogsController.uploadMiddleware, blogsController.saveUpdateBlog);
 router.get("/deleteBlog/:id", blogsController.deleteBlog);
-
 // category
 router.get("/addCategory", categoryController.addCategory);
-router.post("/saveblog", categoryController.saveCategory);
-
+router.post("/saveCategory", categoryController.saveCategory);
+router.get("/updateCategory/:id", categoryController.updateCategory);
+router.post("/saveUpdateCategory/:id", categoryController.saveUpdateCategory);
 // filter
 router.get("/category/:id", categoryController.filter);
 router.post("/search", categoryController.search);
